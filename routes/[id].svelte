040aa84dac7733e094f169240804047b4d1ca249
Ken Mueller <kenmueller0@gmail.com>
2021-11-08 23:43:01 -0800

    game page init

---
+++ routes/[id].svelte
@@ -0,0 +1,21 @@
+<script lang="ts" context="module">
+	export const load: Load = ({ page }) => ({
+		props: { id: page.params.id }
+	})
+</script>
+
+<script lang="ts">
+	import type { Load } from '@sveltejs/kit'
+
+	export let id: string
+</script>
+
+<h1>{id}</h1>
+
+<style lang="scss">
+	@use 'shared/colors';
+
+	h1 {
+		color: colors.$text;
+	}
+</style>
