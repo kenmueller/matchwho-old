804b2b56e4a2396c7ba56b8e661267387f04f6d0
Ken Mueller <kenmueller0@gmail.com>
2021-11-14 11:39:07 -0800

    leader icon

--- components/Game/Players.svelte
+++ components/Game/Players.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import type Game from '../../lib/game'
+	import Leader from '../../icons/Leader.svelte'
 
 	export let game: Game
 </script>
@@ -8,6 +9,9 @@
 	{#each game.players as player (player.id)}
 		<div data-current={game.current.id === player.id}>
 			<p>{player.name}</p>
+			{#if player.leader}
+				<Leader />
+			{/if}
 		</div>
 	{/each}
 </aside>
@@ -19,21 +23,32 @@
 		grid-area: players;
 	}
 
-	div + div {
-		$spacing: 0.8rem;
+	div {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		color: colors.$text;
 
-		margin-top: $spacing;
-		padding-top: $spacing;
-		border-top: 0.125rem solid colors.$border;
+		& + & {
+			$spacing: 0.8rem;
+
+			margin-top: $spacing;
+			padding-top: $spacing;
+			border-top: 0.125rem solid colors.$border;
+		}
 	}
 
 	p {
 		font-weight: 700;
-		color: colors.$text;
 
 		div[data-current='true'] &::after {
 			content: ' (you)';
 			opacity: 0.5;
 		}
+	}
+
+	div > :global(svg) {
+		height: 1.1rem;
+		margin-left: 0.5rem;
 	}
 </style>
