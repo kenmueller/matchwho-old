0006c83850f92a6891bc21b8d97facf3e63d82b5
Ken Mueller <kenmueller0@gmail.com>
2021-11-24 17:54:53 -0500

    logs http error code

--- server/log/error.ts
+++ server/log/error.ts
@@ -2,6 +2,7 @@ import type Log from './index.js'
 import getPrefix from './prefix.js'
 import toString from './string.js'
 import getTrace from './trace.js'
+import HttpError from '../../shared/error/http.js'
 
 const logError: Log = (label, value, id) => {
 	const logTrace = getTrace()
@@ -10,9 +11,11 @@ const logError: Log = (label, value, id) => {
 	console.error(
 		`${getPrefix(label, id)}${
 			value instanceof Error ? value.message : toString(value)
-		}${logTrace && errorTrace ? '\nLog trace:\n' : ''}${logTrace ?? ''}${
-			logTrace && errorTrace ? '\nError trace:\n' : ''
-		}${errorTrace ?? ''}`
+		}${value instanceof HttpError ? ` (code: ${value.code})` : ''}${
+			logTrace && errorTrace ? '\nLog trace:\n' : ''
+		}${logTrace ?? ''}${logTrace && errorTrace ? '\nError trace:\n' : ''}${
+			errorTrace ?? ''
+		}`
 	)
 
 	return value
