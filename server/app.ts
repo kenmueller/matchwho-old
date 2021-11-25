4848e22388a9a727aaf2b7c05cddaa15e0eaf0a2
Ken Mueller <kenmueller0@gmail.com>
2021-11-25 01:14:39 -0500

    compression

--- server/app.ts
+++ server/app.ts
@@ -1,3 +1,6 @@
+import compression from 'compression'
+
+import DEV from './dev.js'
 import { app } from './root.js'
 
 import security from './security/index.js'
@@ -5,6 +8,8 @@ import createGame from './game/create.js'
 import gamePage from './game/page.js'
 import gameMeta from './game/meta.js'
 import './game/stream.js'
+
+if (!DEV) app.use(compression())
 
 app.use(security)
 app.use(createGame)
