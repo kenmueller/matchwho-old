1aa31acc1589b78329626900d3a51ee72660917e
Ken Mueller <kenmueller0@gmail.com>
2021-11-26 18:17:48 -0500

    404 redirects

---
+++ routes/[...path].svelte
@@ -0,0 +1,12 @@
+<script lang="ts" context="module">
+	export const load: Load = () => ({
+		status: HttpErrorCode.TemporaryRedirect,
+		redirect: '/'
+	})
+</script>
+
+<script lang="ts">
+	import type { Load } from '@sveltejs/kit'
+
+	import { HttpErrorCode } from '../shared/error/http.js'
+</script>
